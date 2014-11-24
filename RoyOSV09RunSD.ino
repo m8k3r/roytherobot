eeea7858c3dec11f16401a5f8cf627d7b7cf0b5c
m8k3r <wes@thisportal.com>
2014-11-24 11:03:11 -0500

    Update RoyOSV09RunSD.ino

--- RoyOSV09RunSD.ino
+++ RoyOSV09RunSD.ino
@@ -1,9 +1,11 @@
 /* The code used to create these scripts has come from example scripts included
 with the associated libraries, along with scriptes created by M8k3r to support
-the control of Roy the Robot.  All attribution is included in the library files,
-but not in the test scripts provided.  Outside of the manipulation of example
-code provided with the libraries to make Roy move properly, all attribution and
-software restrictions are included in the libraries and must be honored. */
+the control of Roy the Robot. All attribution is included in the library files,
+but not in the test scripts provided. Outside of the manipulation of example code
+provided with the libraries to make Roy move properly, all attribution and
+software restrictions are included in the libraries and must be honored.
+Most all of the example scripts that have been modified were originally written
+by Limor Fried/Ladyada for Adafruit Industries. */
 
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
